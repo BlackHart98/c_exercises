5ef754c60bfd37fda3504582a254f8d666d8442b
Pius Arhanbhunde <pjacks419@gmail.com>
2025-08-15 22:05:47 +0100

    feat(): Spawning multiple threads

---
+++ threads/multiple_threads.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+#define MAX_THREADS 4
+
+
+void * say_hello(void*);
+
+int main(int argc, char* argv[]){
+    pthread_t some_thread[MAX_THREADS];
+    int thread_idx[MAX_THREADS];
+
+    for (int i = 0; i < MAX_THREADS; i++){
+        thread_idx[i] = i;
+        pthread_create(&some_thread[i], NULL, say_hello, &thread_idx[i]);
+    }
+
+    for (int i = 0; i < MAX_THREADS; i++){
+        pthread_join(some_thread[i], NULL);
+    }
+    return 0;
+}
+
+void * say_hello(void* thread_idx){
+    printf("Hello from thread #%d\n", *((int *) thread_idx));
+    return NULL;
+}
