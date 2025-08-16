236b92d571967734a0af9b80402ce4d84a5e8afd
Pius Arhanbhunde <pjacks419@gmail.com>
2025-08-16 19:48:17 +0100

    feat(): Implemented ring buffer data structure

--- data_structures/ring_buffer.c
+++ data_structures/ring_buffer.c
@@ -19,35 +19,49 @@ int main(int argc, char* argv[]){
     ring_buffer_t my_ring_buffer;
     ring_buffer_create(&my_ring_buffer);
 
+    int my_data[] = {0,3,4,8,2,1,7};
+    ring_buffer_enqueue(&my_ring_buffer, my_data[0]);
 
+    int val;
+    printf("hello world ring buffer front = %d\n", my_ring_buffer.read_head);
 
-    printf("hello world ring buffer front = %d", my_ring_buffer.read_head);
+    ring_buffer_dequeue(&my_ring_buffer);
+
+    if (ring_buffer_peek(&my_ring_buffer, &val) == -1) {
+        printf("Empty ahh\n");
+    } else {
+        printf("hello world, %d\n", val);
+    }
+
     return 0;
 }
+
 
 void ring_buffer_create(ring_buffer_t * ring_buffer){
     ring_buffer->read_head = 0;
     ring_buffer->write_head = 0;
 }
 
+
 int ring_buffer_dequeue(ring_buffer_t * ring_buffer){
-    if ((ring_buffer->read_head + 1) > ring_buffer->write_head) return -1;
-    if (ring_buffer->read_head < BUFFER_SIZE) {
-        ring_buffer->read_head++;
-    } else {
-        ring_buffer->read_head = 0;
-    }
+    if (ring_buffer->read_head == ring_buffer->write_head) return -1;
+    ring_buffer->read_head++;
+    ring_buffer->read_head %= BUFFER_SIZE;
     return 1;
 }
 
 
 int ring_buffer_enqueue(ring_buffer_t * ring_buffer, int item){
+    if ((ring_buffer->write_head + 1) == ring_buffer->read_head) return -1;
+    ring_buffer->buffer[ring_buffer->write_head] = item;
     ring_buffer->write_head++;
-    if (ring_buffer->write_head == BUFFER_SIZE) ring_buffer->write_head = 0;
-    if (ring_buffer->write_head == ring_buffer->read_head) {
-        ring_buffer->write_head--;
-        return -1;
-    }
-    ring_buffer->buffer[ring_buffer->write_head];
+    ring_buffer->write_head %= BUFFER_SIZE;
+    return 1;
+}
+
+
+int ring_buffer_peek(ring_buffer_t * ring_buffer, int * item){
+    if (ring_buffer->write_head == ring_buffer->read_head) return -1;
+    *item = ring_buffer->buffer[ring_buffer->read_head];
     return 1;
 }
