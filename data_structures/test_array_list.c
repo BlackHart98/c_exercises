40d4a1b553fc758f59e1d75d2fedc1cdf50c53c3
Pius Arhanbhunde <pjacks419@gmail.com>
2026-05-09 18:42:08 +0100

    Update test_array_list.c

--- data_structures/test_array_list.c
+++ data_structures/test_array_list.c
@@ -12,12 +12,10 @@ main(int argc, char* argv[])
     array_list_t float_list = array_list_init_capacity(&gpa, float, 200); // float_list: [dynamic]float
     if (NULL != float_list.ptr){
         printf("first init length\n");
-        float item = 4.5;
-        int ret = array_list_append_item_fn(&gpa, &float_list, (char *)&item);
-        if (0 != ret) {
-            printf("Empty!\n");
-            return 1;
-        }
+        float input_array[] = {8, 9, 10, 4.5, 0.7};
+
+        slice_t buf_slice = make_slice((void *)input_array, sizeof(input_array)); 
+        int ret = array_list_append_slice_fn(&gpa, &float_list, buf_slice);
         float *ptr = (float *)(float_list.ptr);
         for (int i = 0; i < float_list.len; i++){
             printf("float_list[%d]: %f\n", i, ptr[i]);
