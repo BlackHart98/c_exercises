fe77ae371f26eedb893759658a9bf9c3c1bd1199
Pius Arhanbhunde <pjacks419@gmail.com>
2026-05-13 18:31:35 +0100

    Update truncate_stdout.c

--- misc/truncate_stdout.c
+++ misc/truncate_stdout.c
@@ -11,14 +11,5 @@ int main(void){
     printf("\033[1A\r"); 
     printf("\033[K");
     fprintf(stdout, "Welcome to terminal!\n");
-
-    // float x = 0.251;
-    // double y = 0.251;
-    // if (x == 0.251){
-    //     printf("Yes they are equal!\n");
-    // }
-    // if (y == x){
-    //     printf("Yes they are equal?\n");
-    // }
     return 0;
 }
