a97626f78e5c86b4d66afa8d1e78ab771d6e39b5
Pius Arhanbhunde <pjacks419@gmail.com>
2026-06-23 11:09:41 +0100

    Added bitset data structure

---
+++ data_structures/test_bitset.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define BITSET_IMPLEMENTATION
+#define WSA_IMPLEMENTATION
+#define ARRAY_LIST_IMPLEMENTATION
+#include "../memory_management/why_so_arena.c"
+#include "../data_structures/bitset.h"
+
+int 
+main(int argc, char* argv[])
+{
+    context_t context = context_init(MB(1), KB(512)); // I need to fix the API boundary of this object!
+    if (NULL == context.allocator.linkedlist || NULL == context.temp_allocator.linkedlist) goto cleanup;
+
+    bitset_t my_bitset = bitset_init(&(context.allocator), 100);
+    if (NULL == my_bitset.bits) goto cleanup;
+
+    printf("Bitset: %zu, %zu\n", my_bitset.size, my_bitset.len);
+    bitset_add(&my_bitset, 30);
+    bitset_add(&my_bitset, 40);
+
+    if (bitset_test(&my_bitset, 30)) {
+        printf("%d in bitset\n", 30);
+        bitset_toggle(&my_bitset, 30);
+    } else printf("%d not in bitset\n", 30);
+
+    if (bitset_test(&my_bitset, 40)) printf("%d in bitset\n", 40);
+    else printf("%d not in bitset\n", 40);
+
+    if (bitset_test(&my_bitset, 30)) printf("%d in bitset\n", 30);
+    else printf("%d not in bitset\n", 30);
+
+    cleanup:
+        context_deinit(&context);
+    return 0;
+}
+
