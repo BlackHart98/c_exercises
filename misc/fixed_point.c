d00e1d8f239435b352eda50e2578aef421dc3c88
Pius Arhanbhunde <pjacks419@gmail.com>
2026-07-03 10:36:05 +0100

    Update fixed_point.c

--- misc/fixed_point.c
+++ misc/fixed_point.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define SIGNIFICANT_NUMBER 1000
+#define FRACTION_BITS 6
+#define SCALE         (1 << FRACTION_BITS)
 
-typedef int16_t fixed_point_t;
+typedef int8_t fixed_point_t; // q1.6
 
 
 fixed_point_t
@@ -12,12 +13,11 @@ float_to_fixed_point(float f);
 float
 fixed_point_to_float(fixed_point_t p);
 
-
 int
 main(void)
 {
-    float f = 1.5457;
-    int16_t fixed = float_to_fixed_point(1.5457);
+    float f = -1.597;
+    fixed_point_t fixed = float_to_fixed_point(f);
     printf("Floating point: %f, corresponding fixed point: %i\n", f, fixed);
     printf("Floating point after conversion: %f\n", fixed_point_to_float(fixed));
     return 0;
@@ -27,12 +27,17 @@ main(void)
 fixed_point_t
 float_to_fixed_point(float f)
 {
-    return (fixed_point_t)(f * SIGNIFICANT_NUMBER + ((f >= 0)? 0.5 : -0.5));
+    size_t size_of_int_rep = sizeof(fixed_point_t);
+    float positive_clip = (1 << ((8 * size_of_int_rep) - 1)) - 1;
+    float negative_clip = -1.0f * (1 << ((8 * size_of_int_rep) - 1));
+    if (f > positive_clip / SCALE) f = positive_clip / SCALE;
+    if (f < negative_clip / SCALE) f = negative_clip / SCALE;
+    return (fixed_point_t)(f * SCALE + ((f >= 0)? 0.5 : -0.5));
 }
 
 
 float
 fixed_point_to_float(fixed_point_t p)
 {
-    return (float)(p)/SIGNIFICANT_NUMBER;
+    return (float)(p) / SCALE;
 }
